1ec83ff074891cca3a90fb69a8257a020f29687a
Hoogy <hoogy7315@gmail.com>
2025-06-02 23:21:49 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template("index.html")
+
+@app.route('/appointment')
+def appointment():
+    return render_template("appointment.html")
+
+if __name__ == '__main__':
+    app.run()
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
     return render_template('home.html')
 
 @app.route('/appointment')
@@ -14,4 +29,6 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
 
